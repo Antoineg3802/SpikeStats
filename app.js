95168dc203863ea59fcb55a6ecd6bc4c46f3f213
Antoineg3802 <antoine.guerin@cyclable.com>
2024-02-02 15:46:10 +0100

    feat: add default routes 404 if route not defined

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Route pour toutes les autres routes non définies
+app.use((req, res) => {
+	res.status(404).send(
+		{
+			success: false,
+			message: 'Endpoint or method invalid. Please check your URL and try again.'
+		}
+	);
+});
+
 const port = process.env.APP_PORT || 8080;
 
 app.listen(port, function () {
