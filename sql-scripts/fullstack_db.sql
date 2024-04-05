664dc50284a019b4f59d81e74a1a7bf1a45bcd7f
Antoineg3802 <antoine.guerin@cyclable.com>
2024-04-05 19:33:41 +0200

    feat: update db

--- sql-scripts/fullstack_db.sql
+++ sql-scripts/fullstack_db.sql
@@ -14,6 +14,88 @@ CREATE TABLE `faults` (
   `oponent_points` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `faults` (`id`, `set_id`, `player_id`, `fault_type_id`, `team_points`, `oponent_points`) VALUES
+(130, 34, 3, 1, 16, 15),
+(131, 34, 4, 2, 18, 12),
+(132, 34, 3, 4, 12, 3),
+(133, 34, 3, 2, 5, 5),
+(134, 34, 4, 3, 17, 1),
+(135, 34, 4, 3, 18, 9),
+(136, 34, 4, 1, 3, 9),
+(137, 34, 3, 1, 8, 6),
+(138, 34, 4, 2, 13, 17),
+(139, 34, NULL, 4, 21, 15),
+(140, 34, NULL, 3, 19, 11),
+(141, 34, NULL, 2, 13, 24),
+(142, 34, NULL, 2, 18, 21),
+(143, 34, NULL, 1, 24, 3),
+(144, 34, NULL, 3, 12, 2),
+(145, 34, NULL, 1, 18, 10),
+(146, 34, NULL, 4, 9, 16),
+(147, 34, NULL, 1, 1, 16),
+(148, 34, NULL, 1, 25, 21),
+(149, 34, NULL, 4, 25, 7),
+(150, 35, 3, 1, 16, 15),
+(151, 35, 4, 2, 18, 12),
+(152, 35, 3, 4, 12, 3),
+(153, 35, 3, 2, 5, 5),
+(154, 35, 4, 3, 17, 1),
+(155, 35, 4, 3, 18, 9),
+(156, 35, 4, 1, 3, 9),
+(157, 35, 3, 1, 8, 6),
+(158, 35, 4, 2, 13, 17),
+(159, 35, NULL, 4, 21, 15),
+(160, 35, NULL, 3, 19, 11),
+(161, 35, NULL, 2, 13, 24),
+(162, 35, NULL, 2, 18, 21),
+(163, 35, NULL, 1, 24, 3),
+(164, 35, NULL, 3, 12, 2),
+(165, 35, NULL, 1, 18, 10),
+(166, 35, NULL, 4, 9, 16),
+(167, 35, NULL, 1, 1, 16),
+(168, 35, NULL, 1, 25, 21),
+(169, 35, NULL, 4, 25, 7),
+(170, 36, 3, 1, 16, 15),
+(171, 36, 4, 2, 18, 12),
+(172, 36, 3, 4, 12, 3),
+(173, 36, 3, 2, 5, 5),
+(174, 36, 4, 3, 17, 1),
+(175, 36, 4, 3, 18, 9),
+(176, 36, 4, 1, 3, 9),
+(177, 36, 3, 1, 8, 6),
+(178, 36, 4, 2, 13, 17),
+(179, 36, NULL, 4, 21, 15),
+(180, 36, NULL, 3, 19, 11),
+(181, 36, NULL, 2, 13, 24),
+(182, 36, NULL, 2, 18, 21),
+(183, 36, NULL, 1, 24, 3),
+(184, 36, NULL, 3, 12, 2),
+(185, 36, NULL, 1, 18, 10),
+(186, 36, NULL, 4, 9, 16),
+(187, 36, NULL, 1, 1, 16),
+(188, 36, NULL, 1, 25, 21),
+(189, 36, NULL, 4, 25, 7),
+(190, 37, 3, 1, 16, 15),
+(191, 37, 4, 2, 18, 12),
+(192, 37, 3, 4, 12, 3),
+(193, 37, 3, 2, 5, 5),
+(194, 37, 4, 3, 17, 1),
+(195, 37, 4, 3, 18, 9),
+(196, 37, 4, 1, 3, 9),
+(197, 37, 3, 1, 8, 6),
+(198, 37, 4, 2, 13, 17),
+(199, 37, NULL, 4, 21, 15),
+(200, 37, NULL, 3, 19, 11),
+(201, 37, NULL, 2, 13, 24),
+(202, 37, NULL, 2, 18, 21),
+(203, 37, NULL, 1, 24, 3),
+(204, 37, NULL, 3, 12, 2),
+(205, 37, NULL, 1, 18, 10),
+(206, 37, NULL, 4, 9, 16),
+(207, 37, NULL, 1, 1, 16),
+(208, 37, NULL, 1, 25, 21),
+(209, 37, NULL, 4, 25, 7);
+
 CREATE TABLE `fault_type` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
@@ -39,6 +121,12 @@ CREATE TABLE `matches` (
   `location` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `matches` (`id`, `date`, `team_id`, `opponent`, `location`) VALUES
+(3, '2024-04-04 06:01:01', 4, 'Vball', "T\'occupe"),
+(4, '2024-05-20 08:27:21', 4, 'Villefranche', 'Villefranche (Palais des sports)'),
+(5, '2024-06-20 08:27:21', 4, 'Villefranche', 'Villefranche (Palais des sports)');
+
+
 CREATE TABLE `points` (
   `id` int(11) NOT NULL,
   `set_id` int(11) NOT NULL,
@@ -48,12 +136,132 @@ CREATE TABLE `points` (
   `oponent_points` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `points` (`id`, `set_id`, `player_id`, `point_type_id`, `team_points`, `oponent_points`) VALUES
+(119, 34, 3, 1, 13, 9),
+(120, 34, 4, 4, 5, 24),
+(121, 34, 3, 1, 14, 16),
+(122, 34, 4, 1, 4, 10),
+(123, 34, 3, 4, 9, 8),
+(124, 34, 4, 1, 7, 6),
+(125, 34, 3, 3, 21, 2),
+(126, 34, 3, 3, 25, 22),
+(127, 34, 4, 2, 3, 18),
+(128, 34, 3, 1, 2, 18),
+(129, 34, 4, 4, 18, 23),
+(130, 34, 4, 5, 1, 25),
+(131, 34, 3, 5, 5, 21),
+(132, 34, 3, 5, 24, 7),
+(133, 34, NULL, 5, 6, 14),
+(134, 34, NULL, 4, 4, 5),
+(135, 34, NULL, 2, 10, 23),
+(136, 34, NULL, 1, 21, 15),
+(137, 34, NULL, 3, 10, 8),
+(138, 34, NULL, 5, 8, 7),
+(139, 34, NULL, 1, 16, 10),
+(140, 34, NULL, 3, 21, 8),
+(141, 34, NULL, 1, 5, 13),
+(142, 34, NULL, 4, 1, 2),
+(143, 34, NULL, 1, 10, 20),
+(144, 34, NULL, 5, 17, 14),
+(145, 34, NULL, 4, 3, 15),
+(146, 34, NULL, 5, 20, 8),
+(147, 35, 3, 1, 13, 9),
+(148, 35, 4, 4, 5, 24),
+(149, 35, 3, 1, 14, 16),
+(150, 35, 4, 1, 4, 10),
+(151, 35, 3, 4, 9, 8),
+(152, 35, 4, 1, 7, 6),
+(153, 35, 3, 3, 21, 2),
+(154, 35, 3, 3, 25, 22),
+(155, 35, 4, 2, 3, 18),
+(156, 35, 3, 1, 2, 18),
+(157, 35, 4, 4, 18, 23),
+(158, 35, 4, 5, 1, 25),
+(159, 35, 3, 5, 5, 21),
+(160, 35, 3, 5, 24, 7),
+(161, 35, NULL, 5, 6, 14),
+(162, 35, NULL, 4, 4, 5),
+(163, 35, NULL, 2, 10, 23),
+(164, 35, NULL, 1, 21, 15),
+(165, 35, NULL, 3, 10, 8),
+(166, 35, NULL, 5, 8, 7),
+(167, 35, NULL, 1, 16, 10),
+(168, 35, NULL, 3, 21, 8),
+(169, 35, NULL, 1, 5, 13),
+(170, 35, NULL, 4, 1, 2),
+(171, 35, NULL, 1, 10, 20),
+(172, 35, NULL, 5, 17, 14),
+(173, 35, NULL, 4, 3, 15),
+(174, 35, NULL, 5, 20, 8),
+(175, 36, 3, 1, 13, 9),
+(176, 36, 4, 4, 5, 24),
+(177, 36, 3, 1, 14, 16),
+(178, 36, 4, 1, 4, 10),
+(179, 36, 3, 4, 9, 8),
+(180, 36, 4, 1, 7, 6),
+(181, 36, 3, 3, 21, 2),
+(182, 36, 3, 3, 25, 22),
+(183, 36, 4, 2, 3, 18),
+(184, 36, 3, 1, 2, 18),
+(185, 36, 4, 4, 18, 23),
+(186, 36, 4, 5, 1, 25),
+(187, 36, 3, 5, 5, 21),
+(188, 36, 3, 5, 24, 7),
+(189, 36, NULL, 5, 6, 14),
+(190, 36, NULL, 4, 4, 5),
+(191, 36, NULL, 2, 10, 23),
+(192, 36, NULL, 1, 21, 15),
+(193, 36, NULL, 3, 10, 8),
+(194, 36, NULL, 5, 8, 7),
+(195, 36, NULL, 1, 16, 10),
+(196, 36, NULL, 3, 21, 8),
+(197, 36, NULL, 1, 5, 13),
+(198, 36, NULL, 4, 1, 2),
+(199, 36, NULL, 1, 10, 20),
+(200, 36, NULL, 5, 17, 14),
+(201, 36, NULL, 4, 3, 15),
+(202, 36, NULL, 5, 20, 8),
+(203, 37, 3, 1, 13, 9),
+(204, 37, 4, 4, 5, 24),
+(205, 37, 3, 1, 14, 16),
+(206, 37, 4, 1, 4, 10),
+(207, 37, 3, 4, 9, 8),
+(208, 37, 4, 1, 7, 6),
+(209, 37, 3, 3, 21, 2),
+(210, 37, 3, 3, 25, 22),
+(211, 37, 4, 2, 3, 18),
+(212, 37, 3, 1, 2, 18),
+(213, 37, 4, 4, 18, 23),
+(214, 37, 4, 5, 1, 25),
+(215, 37, 3, 5, 5, 21),
+(216, 37, 3, 5, 24, 7),
+(217, 37, NULL, 5, 6, 14),
+(218, 37, NULL, 4, 4, 5),
+(219, 37, NULL, 2, 10, 23),
+(220, 37, NULL, 1, 21, 15),
+(221, 37, NULL, 3, 10, 8),
+(222, 37, NULL, 5, 8, 7),
+(223, 37, NULL, 1, 16, 10),
+(224, 37, NULL, 3, 21, 8),
+(225, 37, NULL, 1, 5, 13),
+(226, 37, NULL, 4, 1, 2),
+(227, 37, NULL, 1, 10, 20),
+(228, 37, NULL, 5, 17, 14),
+(229, 37, NULL, 4, 3, 15),
+(230, 37, NULL, 5, 20, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `point_type`
+--
+
 CREATE TABLE `point_type` (
   `id` int(11) NOT NULL,
-  `point_type` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `point_type` (`id`, `point_type`) VALUES
+INSERT INTO `point_type` (`id`, `name`) VALUES
 (1, 'Attaque'),
 (2, 'Bloc'),
 (3, 'Service ace'),
@@ -78,8 +286,14 @@ CREATE TABLE `sets` (
   `end_set` datetime NOT NULL,
   `team_score` int(11) NOT NULL,
   `opponent_score` int(11) NOT NULL,
-  `winner` varchar(25) NOT NULL
+  `winner` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `sets` (`id`, `match_id`, `number_set`, `start_set`, `end_set`, `team_score`, `opponent_score`, `winner`) VALUES
+(34, 3, 1, '2024-02-21 14:00:00', '2024-02-21 14:45:00', 25, 23, 1),
+(35, 3, 2, '2024-02-21 14:00:00', '2024-02-21 14:45:00', 25, 27, 1),
+(36, 3, 3, '2024-02-21 14:00:00', '2024-02-21 14:45:00', 25, 23, 0),
+(37, 3, 4, '2024-02-21 14:00:00', '2024-02-21 14:45:00', 25, 23, 1);
 
 CREATE TABLE `teams` (
   `id` int(11) NOT NULL,
@@ -98,6 +312,7 @@ CREATE TABLE `teams_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `teams_users` (`user_id`, `team_id`) VALUES
+(2, 4),
 (3, 4),
 (4, 4);
 
@@ -114,7 +329,9 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `password`, `mail`, `role_id
 (1, 'Antoine', 'Guerin', '$2b$10$XBZwNgezncURTtumhDyB4enNVxRg.Kg.B.6B4c0kEQM3dxGj9kH.2', 'antoineg3802@gmail.com', 1),
 (2, 'ta', 'ta', '$2b$10$isxlRBpoAXyxL1QAZfqmL.l.CHyZcQVf4Qg63mJS5YIATOpqPLFaG', 'tata@gmail.com', 3),
 (3, 'tu', 'tu', '$2b$10$22P5ZnzeQjoStE2ZxrRv4eYkqRZzptRmnB6iwHjc5uidxanlVCC3m', 'tutu@gmail.com', 2),
-(4, 'to', 'to', '$2b$10$K78DLf8NgGCYSCBWFwW9ROfbOiXozEgd64YA9WKFi/dJiUR5I9M4u', 'toto@gmail.com', 2);
+(4, 'to', 'to', '$2b$10$K78DLf8NgGCYSCBWFwW9ROfbOiXozEgd64YA9WKFi/dJiUR5I9M4u', 'toto@gmail.com', 2),
+(5, 'te', 'te', '$2b$10$0E.1Y.8v24G.J6T2CzoBcuOoT.qPgrDbIQm7eDmlpB45TzVFlDyLu', 'tete@gmail.com', 2);
+
 
 CREATE TABLE `users_matches` (
   `user_id` int(11) NOT NULL,
@@ -123,9 +340,9 @@ CREATE TABLE `users_matches` (
 
 ALTER TABLE `faults`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `set_id` (`set_id`),
   ADD KEY `player_id` (`player_id`),
-  ADD KEY `fk_fault_type` (`fault_type_id`);
+  ADD KEY `fault_type_id` (`fault_type_id`),
+  ADD KEY `faults_ibfk_1` (`set_id`);
 
 ALTER TABLE `fault_type`
   ADD PRIMARY KEY (`id`);
@@ -137,9 +354,9 @@ ALTER TABLE `matches`
 
 ALTER TABLE `points`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `set_id` (`set_id`),
   ADD KEY `player_id` (`player_id`),
-  ADD KEY `fk_point_type` (`point_type_id`);
+  ADD KEY `point_type_id` (`point_type_id`),
+  ADD KEY `fk_point_set` (`set_id`);
 
 ALTER TABLE `point_type`
   ADD PRIMARY KEY (`id`);
@@ -150,7 +367,7 @@ ALTER TABLE `roles`
 ALTER TABLE `sets`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `match_id` (`match_id`);
+  ADD KEY `fk_set_match` (`match_id`);
 
 ALTER TABLE `teams`
   ADD PRIMARY KEY (`id`),
@@ -169,41 +386,50 @@ ALTER TABLE `users_matches`
   ADD KEY `user_id` (`user_id`),
   ADD KEY `match_id` (`match_id`);
 
+ALTER TABLE `faults`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=210;
+
 ALTER TABLE `fault_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 ALTER TABLE `matches`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+ALTER TABLE `points`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=231;
 
 ALTER TABLE `point_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 ALTER TABLE `sets`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 ALTER TABLE `teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 ALTER TABLE `faults`
-  ADD CONSTRAINT `faults_ibfk_1` FOREIGN KEY (`set_id`) REFERENCES `sets` (`id`),
+  ADD CONSTRAINT `faults_ibfk_1` FOREIGN KEY (`set_id`) REFERENCES `sets` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `faults_ibfk_2` FOREIGN KEY (`player_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `fk_fault_type` FOREIGN KEY (`fault_type_id`) REFERENCES `fault_type` (`id`);
+  ADD CONSTRAINT `fk_fault_set` FOREIGN KEY (`set_id`) REFERENCES `sets` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_fault_type` FOREIGN KEY (`fault_type_id`) REFERENCES `fault_type` (`id`) ON UPDATE CASCADE;
 
 ALTER TABLE `matches`
   ADD CONSTRAINT `matches_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`);
 
 ALTER TABLE `points`
-  ADD CONSTRAINT `fk_point_type` FOREIGN KEY (`point_type_id`) REFERENCES `point_type` (`id`),
+  ADD CONSTRAINT `fk_point_set` FOREIGN KEY (`set_id`) REFERENCES `sets` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `points_ibfk_1` FOREIGN KEY (`set_id`) REFERENCES `sets` (`id`),
-  ADD CONSTRAINT `points_ibfk_2` FOREIGN KEY (`player_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `points_ibfk_2` FOREIGN KEY (`player_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `points_ibfk_3` FOREIGN KEY (`point_type_id`) REFERENCES `point_type` (`id`) ON UPDATE CASCADE;
 
 ALTER TABLE `sets`
+  ADD CONSTRAINT `fk_set_match` FOREIGN KEY (`match_id`) REFERENCES `matches` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `sets_ibfk_1` FOREIGN KEY (`match_id`) REFERENCES `matches` (`id`);
 
 ALTER TABLE `teams`
