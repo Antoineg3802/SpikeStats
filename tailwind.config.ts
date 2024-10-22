955bfee8aed6278d8afeaa1aa446fb344441e5b6
Antoineg3802 <antoine.guerin@cyclable.com>
2024-10-22 16:04:56 +0200

    feat: add default tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  darkMode: 'selector',
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	darkMode: 'selector',
+	theme: {
+		extend: {
+			colors: {
+				white: "#fff",
+				black: "#000000",
+				darkBlue: "#0B2647",
+				lightOrange: "#feb272",
+				orange: "#ff7f32",
+				red: "#dc3545",
+				lightRed: "#f8d7da",
+				transparent: "rgba(0,0,0,0.5)",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
