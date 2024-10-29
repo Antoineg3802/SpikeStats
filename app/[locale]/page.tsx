98c552e957efa77bc40b773d22bc6da8579658d1
Antoineg3802 <antoine.guerin@cyclable.com>
2024-10-29 16:13:26 +0100

    fix: next-internationalization

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,11 +3,9 @@ import { signIn, signOut } from "next-auth/react";
 import LocaleSelector from "@/components/atoms/LocaleSelector";
 import Navbar from "@/components/organisms/Navbar";
 import { auth } from "@/lib/auth/auth";
-import { getI18n } from "../../locales/server";
 
 export default async function Home() {
 	const session = await auth()
-	const t = await getI18n();
 
 	return (
 		<div>
