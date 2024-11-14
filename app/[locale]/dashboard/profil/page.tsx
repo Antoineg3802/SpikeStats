72dbd6712fd1c775d0ac05d86241757bea28bd74
Antoineg3802 <antoine.guerin@cyclable.com>
2024-11-14 09:49:39 +0100

    fix: correct profil page

--- app/[locale]/dashboard/profil/page.tsx
+++ app/[locale]/dashboard/profil/page.tsx
@@ -1,30 +1,11 @@
 "use client"
 
-import { useServerAction } from "zsa-react"
 import { getUsers } from "@/actions/users.action"
 import { IconLoader2 } from "@tabler/icons-react"
 import { useEffect, useRef } from "react"
 
 export default function Page() {
-    const { isPending, isSuccess, isError, execute, data, error } = useServerAction(getUsers)
-    const users = data || []
-    const hasFetchedData = useRef(false);
-
-    useEffect(() => {
-        if (!hasFetchedData.current && !isPending && !isSuccess) {
-            execute();
-            hasFetchedData.current = true;
-        }
-    }, [isPending, isSuccess]);
-
     return <div>
-        {isPending && <IconLoader2 className="animate-spin" />}
-        {isSuccess && users.length === 0 && <p>No users found</p>}
-        {isSuccess && users.length > 0 && 
-            users.map((user) => {
-                return <p key={user.id}>{user.email}</p>
-            })
-        }
-        {isError && <p>{error.message}</p>}
+        UserProfil (Not implemented)
     </div>
 }
