be51c3c4c62ad796926adfd0b077ae11daf4ca30
Antoineg3802 <antoine.guerin@cyclable.com>
2024-11-14 17:09:40 +0100

    fix: treat ts erreor without ts-ignore

--- app/[locale]/pricing/page.tsx
+++ app/[locale]/pricing/page.tsx
@@ -11,22 +11,22 @@ export default async function Page() {
         expand: ['data.default_price'],
     });
 
-    console.log(products)
-
     // order products by price reccuring interval
-    products.data = products.data.sort((a: Stripe.Product, b) => {
-        // @ts-ignore
-        if (a.default_price?.recurring.interval > b.default_price?.recurring.interval) {
-            return 1;
-        }
-        // @ts-ignore
-        if (a.default_price?.recurring.interval < b.default_price?.recurring.interval) {
-            return -1;
+    products.data = products.data.sort((a: Stripe.Product, b: Stripe.Product) => {
+        const default_price_a = a.default_price as Stripe.Price;
+        const default_price_b = b.default_price as Stripe.Price;
+        if (default_price_a && default_price_b) {
+            if (default_price_a.recurring?.interval && default_price_b.recurring?.interval && default_price_a.recurring.interval > default_price_b.recurring.interval) {
+                return 1;
+            }
+            if (default_price_a.recurring?.interval && default_price_b.recurring?.interval && default_price_a.recurring.interval < default_price_b.recurring.interval) {
+                return -1;
+            }
         }
         return 0;
     });
 
-    return <div className=''>
+    return <div>
         <Navbar session={session} />
         <div className="w-full h-full flex gap-12 justify-center">
             {products.data.map((product, index) => (
@@ -34,10 +34,19 @@ export default async function Page() {
                     <h1 className='font-semibold mb-4'>{product.name}</h1>
                     {product.description && <p>{product.description}</p>}
                     {product.default_price != null &&
-                        <div>
-                            {/* @ts-ignore */}
-                            <span className="text-4xl font-bold">{product.default_price.unit_amount / 100} €</span> <span className='flex-col'>par {product.default_price.recurring.interval == "month" ? "mois" : "an"}</span>
-                        </div>
+                        (() => {
+                            const price = product.default_price as Stripe.Price;
+                            return (
+                                price.unit_amount != null && (
+                                    <div>
+                                        <span className="text-4xl font-bold">{price.unit_amount / 100} €</span>
+                                        <span className='flex-col'>
+                                            par {price.recurring?.interval === "month" ? "mois" : "an"}
+                                        </span>
+                                    </div>
+                                )
+                            );
+                        })()
                     }
                     <button className='p-2 mt-4 bg-lightOrange rounded-lg hover:bg-lightOrange/80'>Démarrer l'essai</button>
                     <p className='mt-3'>Fonctionnnalités :</p>
