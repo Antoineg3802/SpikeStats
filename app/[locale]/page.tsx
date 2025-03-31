de1479da2d28b9cfb4147cdb7bd5631726c18581
Antoineg3802 <antoine.guerin@cyclable.com>
2025-03-31 10:30:20 +0200

    feat: psuh details

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,6 @@
 import LocaleSelector from "@/components/atoms/LocaleSelector";
 import Navbar from "@/components/organisms/Navbar";
 import { auth } from "@/lib/auth/auth";
-import Image from "next/image";
 
 export default async function Home() {
 	const session = await auth()
