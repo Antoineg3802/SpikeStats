b48dfe916539a45f3a776d785fc7eea01d108c10
Antoineg3802 <antoine.guerin@cyclable.com>
2025-04-02 17:05:12 +0200

    feat: add premices

--- app/[locale]/dashboard/teams/page.tsx
+++ app/[locale]/dashboard/teams/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-    return <p>Not Implemented</p>
+import DashboardPageTitle from "@/components/atoms/Titles/DashboardPageTitle";
+import NavbarDashboard from "@/components/organisms/NavbarDashboard";
+import DashboardPage from "@/components/pages/DashboardPage";
+import { Session } from "@/datas/session";
+import { auth } from "@/lib/auth/auth";
+import { useSession } from "next-auth/react";
+
+export default async function Page() {
+    const session = await auth();
+    return <DashboardPage session={session}>
+        <DashboardPageTitle title="tutu" />
+        <p>
+            This page is under construction. <br />
+        </p>
+    </DashboardPage>
 }
