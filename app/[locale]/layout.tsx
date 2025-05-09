cb5af1699554cfb7049c1d30ede01379e65652ba
Antoine GUERIN <antoineg3802@gmail.com>
2025-05-09 11:07:12 +0200

    feat: add dev vizualizer

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,9 +3,9 @@ import "./globals.css";
 
 import { auth } from "@/lib/auth/auth";
 import { ReactElement } from "react";
-import { QueryClient } from "react-query";
 import Providers from "./providers";
 import { GeistSans } from "geist/font/sans";
+import DevVisualizer from "@/components/atoms/DevVisualizer";
 
 export default async function Layout({ children, params }: { children: ReactElement, params: Promise<{ locale: string }> }) {
 	const { locale } = await params
@@ -14,6 +14,7 @@ export default async function Layout({ children, params }: { children: ReactElem
 	return (
 		<html lang={locale} suppressHydrationWarning>
 			<body className={`${GeistSans.className} text-foreground antialiased min-h-screen min-w-screen bg-background`}>
+				{process.env.NEXT_PUBLIC_DEV_MODE == "true" && <DevVisualizer/>}
 				<Providers locale={locale} session={session}>
 					{children}
 				</Providers>
---
+++ src/components/atoms/DevVisualizer.tsx
@@ -0,0 +1,7 @@
+export default function DevVisualizer() {
+    return (
+        <div className="w-dvw h-fit py-1 bg-green-300 flex justify-center">
+            <p className="text-white">Site en  developpement</p>
+        </div>
+    )
+}
