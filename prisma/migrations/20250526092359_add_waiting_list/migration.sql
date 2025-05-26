f4252986d486a84b6be01bf5f0f615f02462373c
Antoine GUERIN <antoineg3802@gmail.com>
2025-05-26 11:44:44 +0200

    fix: correct migration

--- prisma/migrations/20250526092359_add_waiting_list/migration.sql
+++ prisma/migrations/20250526092359_add_waiting_list/migration.sql
@@ -4,9 +4,15 @@ CREATE TABLE "WaitingList" (
     "email" TEXT NOT NULL,
     "name" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "confirmToken" TEXT,
+    "tokenExpiresAt" TIMESTAMP(3),
 
     CONSTRAINT "WaitingList_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "WaitingList_email_key" ON "WaitingList"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WaitingList_confirmToken_key" ON "WaitingList"("confirmToken");
